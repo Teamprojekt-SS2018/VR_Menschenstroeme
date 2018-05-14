6c3dc39839a0a8a510401e15eaea67c720bde8b4
me431zey <32568508+me431zey@users.noreply.github.com>
2018-05-14 16:23:07 +0200

    Mit tischen

--- VRMenschenstroeme/Assets/03_Scripts/Parser/Helper.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Helper {
-    /* Helper */
-    public static string[] SplitWhitespace(string input)
-    {
-        string pattern = @"\t+| +";
-        return System.Text.RegularExpressions.Regex.Split(input, pattern);
-    }
-}
