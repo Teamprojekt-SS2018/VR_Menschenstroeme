79ea81c03065dd207eb48719148e0a706a6cb73b
Nick Strehle <ni431str@htwg-konstanz.de>
2018-06-07 10:50:48 +0200

    Move Update
    Move update

--- VRMenschenstroeme/Assets/03_Scripts/Movement/MoveScript.cs
+++ VRMenschenstroeme/Assets/03_Scripts/Movement/MoveScript.cs
@@ -17,13 +17,15 @@ public class MoveScript : MonoBehaviour {
         rend = gameObject.GetComponent<MeshRenderer>();
     }
     // Update is called once per frame
-    void Update () {
+    void FixedUpdate () {
         if (this.posistions != null)
         {
             if (time <= cb.maxTime - this.moveTime)
             {
                 this.time += Time.deltaTime;
-                this.transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, posistions[(int)(this.time / this.moveTime/*conf_time*/) + 1].position * conf.Length + new Vector3(0, 1f, 0), this.moveTime/*conf_time*/ * Time.deltaTime);
+                float distance = Vector3.Distance(this.transform.localPosition, posistions[(int)(this.time / this.moveTime/*conf_time*/) + 1].position * conf.Length + new Vector3(0, 1f, 0));
+                
+                this.transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, posistions[(int)(this.time / this.moveTime/*conf_time*/) + 1].position * conf.Length + new Vector3(0, 1f, 0),distance / (this.moveTime/Time.fixedDeltaTime));
                 rend.material.color = Color.Lerp(low_density, high_density, posistions[(int)(this.time / this.moveTime/*conf_time*/)].density);
                 if (!this.posistions[(int)(this.time / this.moveTime/*conf_time*/)].activated)
                     this.gameObject.SetActive(false);
