9115b5ae338cee496df576f57a5f28fc555afaaf
me431zey <32568508+me431zey@users.noreply.github.com>
2018-06-08 15:33:28 +0200

    ReAdd

---
+++ VRMenschenstroeme/Assets/03_Scripts/MeshCreator/TeleportAreaMeshcreator.cs
@@ -0,0 +1,157 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: An area that the player can teleport to
+//
+//=============================================================================
+
+using UnityEngine;
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Valve.VR.InteractionSystem
+{
+    //-------------------------------------------------------------------------
+    public class TeleportAreaMeshcreator : TeleportMarkerBase
+    {
+        //Public properties
+        public Bounds meshBounds { get; private set; }
+
+        //Private data
+        public MeshRenderer areaMesh;
+        private int tintColorId = 0;
+        private Color visibleTintColor = Color.clear;
+        private Color highlightedTintColor = Color.clear;
+        private Color lockedTintColor = Color.clear;
+        private bool highlighted = false;
+
+
+        //-------------------------------------------------
+        public void Init()
+        {
+
+            tintColorId = Shader.PropertyToID("_TintColor");
+
+            CalculateBounds();
+
+            visibleTintColor = Teleport.instance.areaVisibleMaterial.GetColor(tintColorId);
+            highlightedTintColor = Teleport.instance.areaHighlightedMaterial.GetColor(tintColorId);
+            lockedTintColor = Teleport.instance.areaLockedMaterial.GetColor(tintColorId);
+        }
+
+
+        //-------------------------------------------------
+        public override bool ShouldActivate(Vector3 playerPosition)
+        {
+            return true;
+        }
+
+
+        //-------------------------------------------------
+        public override bool ShouldMovePlayer()
+        {
+            return true;
+        }
+
+
+        //-------------------------------------------------
+        public override void Highlight(bool highlight)
+        {
+            if (!locked)
+            {
+                highlighted = highlight;
+
+                if (highlight)
+                {
+                    areaMesh.material = Teleport.instance.areaHighlightedMaterial;
+                }
+                else
+                {
+                    areaMesh.material = Teleport.instance.areaVisibleMaterial;
+                }
+            }
+        }
+
+
+        //-------------------------------------------------
+        public override void SetAlpha(float tintAlpha, float alphaPercent)
+        {
+            Color tintedColor = GetTintColor();
+            tintedColor.a *= alphaPercent;
+            areaMesh.material.SetColor(tintColorId, tintedColor);
+        }
+
+
+        //-------------------------------------------------
+        public override void UpdateVisuals()
+        {
+            if (locked)
+            {
+                areaMesh.material = Teleport.instance.areaLockedMaterial;
+            }
+            else
+            {
+                areaMesh.material = Teleport.instance.areaVisibleMaterial;
+            }
+        }
+
+
+        //-------------------------------------------------
+        public void UpdateVisualsInEditor()
+        {
+            areaMesh = GetComponent<MeshRenderer>();
+
+            if (locked)
+            {
+                areaMesh.sharedMaterial = Teleport.instance.areaLockedMaterial;
+            }
+            else
+            {
+                areaMesh.sharedMaterial = Teleport.instance.areaVisibleMaterial;
+            }
+        }
+
+
+        //-------------------------------------------------
+        private bool CalculateBounds()
+        {
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+            if (meshFilter == null)
+            {
+                return false;
+            }
+
+            Mesh mesh = meshFilter.mesh;
+            if (mesh == null)
+            {
+                return false;
+            }
+            mesh.RecalculateBounds();
+
+            meshBounds = mesh.bounds;
+            return true;
+        }
+
+
+        //-------------------------------------------------
+        private Color GetTintColor()
+        {
+            if (locked)
+            {
+                return lockedTintColor;
+            }
+            else
+            {
+                if (highlighted)
+                {
+                    return highlightedTintColor;
+                }
+                else
+                {
+                    return visibleTintColor;
+                }
+            }
+        }
+    }
+}
---
+++ VRMenschenstroeme/Assets/03_Scripts/SimulationObjects/LoadPrefabsInResources.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadPrefabsInResources : MonoBehaviour {
+
+    private GameObject[] prefabs;
+    [Range(0.001f, 5f)]
+    public float emptySize = 0.1f;
+    List<GameObject> ShelfGameObjects = new List<GameObject>();
+    private int currentStartIndexPrefabs = 0;
+    private List<GameObject> currentObjOnShelf = new List<GameObject>();
+
+
+    // Use this for initialization
+    void Start() {
+        prefabs = Resources.LoadAll<GameObject>("");
+
+        ShelfGameObjects.Add(this.gameObject.transform.GetChild(0).gameObject);
+        ShelfGameObjects.Add(this.gameObject.transform.GetChild(1).gameObject);
+        ShelfGameObjects.Add(this.gameObject.transform.GetChild(2).gameObject);
+        ShelfGameObjects.Add(this.gameObject.transform.GetChild(3).gameObject);
+        ShelfGameObjects.Add(this.gameObject.transform.GetChild(4).gameObject);
+        ShelfGameObjects.Add(this.gameObject.transform.GetChild(5).gameObject);
+
+        AddNextGameObjectsToShelf();
+    }
+
+    public void AddNextGameObjectsToShelf() {
+        DeleteCurrentObjOnShelf();
+        foreach (GameObject shelfGameObj in ShelfGameObjects) {
+            if (prefabs != null && prefabs.Length > 0) {
+                currentObjOnShelf.Add(CreateSimulationGameObject(shelfGameObj, prefabs[currentStartIndexPrefabs]));
+                ++currentStartIndexPrefabs;
+                if (currentStartIndexPrefabs >= prefabs.Length) {
+                    currentStartIndexPrefabs = 0;
+                }
+            }
+        }
+    }
+
+    public void AddPreviousGameObjectsToShelf() {
+        DeleteCurrentObjOnShelf();
+        foreach (GameObject shelfGameObj in ShelfGameObjects) {
+            if (prefabs != null && prefabs.Length > 0) {
+                currentObjOnShelf.Add(CreateSimulationGameObject(shelfGameObj, prefabs[currentStartIndexPrefabs]));
+                --currentStartIndexPrefabs;
+                if (currentStartIndexPrefabs < 0) {
+                    currentStartIndexPrefabs = prefabs.Length - 1;
+                }
+            }
+        }
+
+    }
+
+    private void DeleteCurrentObjOnShelf() {
+        foreach (GameObject item in currentObjOnShelf) {
+            if (item != null) { Destroy(item); }
+        }
+    }
+
+
+    private GameObject CreateSimulationGameObject(GameObject shelfObject, GameObject prefab) {
+        MeshFilter shelfObject_mf = shelfObject.GetComponent<MeshFilter>();
+        if (shelfObject_mf != null) {
+            Bounds bounds = shelfObject_mf.mesh.bounds;
+
+            float max = bounds.extents.x;
+            if (max < bounds.extents.y)
+                max = bounds.extents.y;
+            if (max < bounds.extents.z)
+                max = bounds.extents.z;
+
+            float scale = (emptySize * 0.5f) / max;
+
+            GameObject newObject = Instantiate(prefab, shelfObject.transform.position, prefab.transform.rotation);
+
+            if (newObject.GetComponent<Rigidbody>() == null) {
+                newObject.AddComponent<Rigidbody>();
+            }
+            if (newObject.GetComponent<MeshCollider>() == null) {
+                newObject.AddComponent<MeshCollider>();
+            }
+            newObject.GetComponent<MeshCollider>().convex = true;
+            newObject.GetComponent<Rigidbody>().isKinematic = false;
+            newObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            newObject.transform.SetParent(shelfObject.transform);
+            newObject.AddComponent<VRInteractableObject>();
+            newObject.tag = "SimulationObject";
+
+            newObject.transform.localScale = new Vector3(scale, scale, scale);
+            return newObject;
+        }
+        return new GameObject();
+    }
+}
