0dca91a31ce8c91e44e32021add91e992e93087e
me431zey <32568508+me431zey@users.noreply.github.com>
2018-06-11 15:42:34 +0200

    SaveLoad eingebaut funnktioniert

--- VRMenschenstroeme/Assets/03_Scripts/Save_Load/SaveLoad_PlacedObjects.cs
+++ VRMenschenstroeme/Assets/03_Scripts/Save_Load/SaveLoad_PlacedObjects.cs
@@ -1,13 +1,62 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using UnityEditor;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
-public class SaveLoad_PlacedObjects : MonoBehaviour {
 
+
+[Serializable]
+public class SaveData {
+    public string PrefabName;
+    public string Id;
+    public Vector3 Position;
+    public Vector3 Scale;
+    public Quaternion Rotation;
+
+    public override string ToString() {
+        return PrefabName + "_" + Id + "_Position: (" + Position.x + ", " + Position.y + ", " + Position.z + ")" + "_Scale: (" + Scale.x + ", " + Scale.y + ", " + Scale.z + ")";
+    }
+}
+
+public static class JsonHelper {
+    public static T[] FromJson<T>(string json) {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.Items;
+    }
+
+    public static string ToJson<T>(T[] array) {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.Items = array;
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    public static string ToJson<T>(T[] array, bool prettyPrint) {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.Items = array;
+        return JsonUtility.ToJson(wrapper, prettyPrint);
+    }
+
+    [Serializable]
+    private class Wrapper<T> {
+        public T[] Items;
+    }
+}
+
+
+public class SaveLoad_PlacedObjects : MonoBehaviour {
     // Declare and initialize a new List of GameObjects called currentCollisions.
     List<GameObject> objectsOnTable = new List<GameObject>();
     public string SaveGameName = "SimulationSave";
+    public GameObject Map;
+    private string SavePath = "";
+    private GameObject[] prefabs;
+
+    // Use this for initialization
+    void Start() {
+        prefabs = Resources.LoadAll<GameObject>("0_SimulationObjects\\");
+        SavePath = Application.dataPath + "/05_SaveData/";
+    }
 
     void OnCollisionEnter(Collision col) {
         // Add the GameObject collided with to the list.
@@ -20,12 +69,62 @@ public class SaveLoad_PlacedObjects : MonoBehaviour {
     }
 
     public void save() {
-        GameObject map = transform.Find("Map").gameObject;
-        foreach (GameObject item in objectsOnTable) {
-            item.transform.SetParent(map.transform);
+        Debug.Log(SavePath);
+
+
+        SaveData[] saveDataInstance = new SaveData[objectsOnTable.Count];
+
+        for (int i = 0; i < objectsOnTable.Count; i++) {
+            objectsOnTable[i].transform.SetParent(Map.transform);
+            string _name = objectsOnTable[i].name;
+            saveDataInstance[i] = new SaveData();
+            saveDataInstance[i].PrefabName = _name.Substring(0, _name.IndexOf("(Clone)"));
+            saveDataInstance[i].Id = _name.Substring(_name.IndexOf("(Clone)_") + 8);
+            saveDataInstance[i].Scale = objectsOnTable[i].transform.localScale;
+            saveDataInstance[i].Position = objectsOnTable[i].transform.localPosition;
+            saveDataInstance[i].Rotation = objectsOnTable[i].transform.localRotation;
         }
+
+        //Convert to Jason
+        string playerToJason = JsonHelper.ToJson(saveDataInstance, true);
+        Debug.Log(playerToJason);
+
+        File.WriteAllText(SavePath + SaveGameName + ".json", playerToJason);
+
     }
 
     public void load() {
+        string jsonString = File.ReadAllText(SavePath + SaveGameName + ".json");
+        for (int i = 0; i < objectsOnTable.Count; i++) {
+            Destroy(objectsOnTable[i]);
+        }
+        objectsOnTable = new List<GameObject>();
+
+
+        if (jsonString.Length > 1) {
+            SaveData[] player = JsonHelper.FromJson<SaveData>(jsonString);
+            foreach (SaveData item in player) {
+
+                GameObject prefab = prefabs.First(x => x.name == item.PrefabName);
+
+                GameObject newObject = Instantiate(prefab, item.Position, item.Rotation);
+
+                newObject.AddComponent<Rigidbody>();
+                newObject.AddComponent<MeshCollider>();
+                newObject.GetComponent<MeshCollider>().convex = true;
+                newObject.GetComponent<Rigidbody>().isKinematic = false;
+                newObject.transform.SetParent(Map.transform);
+                newObject.AddComponent<VRInteractableObject>();
+                newObject.GetComponent<VRInteractableObject>().clonedObject = true;
+                newObject.tag = "SimulationObject";
+
+                newObject.transform.localScale = item.Scale;
+                newObject.transform.localRotation = item.Rotation;
+                newObject.transform.localPosition = item.Position;
+                newObject.name = item.PrefabName + "(Clone)_" + item.Id;
+                objectsOnTable.Add(newObject);
+            }
+        }
     }
 }
+
